6ca318d46d1d5db6e4fd388d4ced78b7bc2909a0
Bekzod Tolipov <btolipov110@gmail.com>
2023-05-08 08:58:56 -0500

    Fix caching

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def split_chars(text):
     return " ".join(list(text))
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache_data
 def load_model_from_storage():
     download_model()
     unzip_data()
